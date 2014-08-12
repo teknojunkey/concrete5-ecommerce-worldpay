158d299b2055541d5d0be2b298cf23d2c898f237
ewen <ewen.lamont@crocodile-communication.com>
2014-08-12 12:23:44 +0100

    Update footer.php
    Return to the checkout finish page - tested on $pkgVersion = '2.8.13';

--- elements/payment_result/footer.php
+++ elements/payment_result/footer.php
@@ -4,7 +4,7 @@ defined('C5_EXECUTE') or die(_("Access Denied."));
 <br />
 
 <div>
-	<h2 style="text-align: center;"><a href="<?php echo BASE_URL . DIR_REL; ?>"><?php echo t("Return to website"); ?></a></h2>
+	<h2 style="text-align: center;"><a href="<?php echo BASE_URL . DIR_REL; ?>index.php/checkout/finish/"><?php echo t("Return to website"); ?></a></h2>
 </div>
 
 </div>
